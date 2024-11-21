e759799f2654bd6abbde6621347fd2790b5fcffc
Tarun-Nautiyal <Tarun.nautiyal30@gmail.com>
2024-11-21 22:10:42 +0530

    User Authorization and Authentication

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+const bcrypt = require('bcrypt');
+const app = express();
+const PORT = 3000;
+
+mongoose.connect('mongodb://localhost:27017/testdb', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+const logger = (req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();                 
+};
+
+app.use(logger);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const existingUser = await User.findOne({ username });
+
+        if (!existingUser) {
+            const hashedPassword = await bcrypt.hash(password, 10);
+            const newUser = new User({ username, password: hashedPassword });
+            await newUser.save();
+            res.send('Account created successfully!');
+        } else {
+            const match = await bcrypt.compare(password, existingUser.password);
+            if (match) {
+                res.redirect('/student-login');
+            } else {
+                res.status(401).send('Authentication failed. Incorrect password.');
+            }
+        }
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.get('/student-login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'student-login.html'));
+});
+
+const errorHandler = (err, req, res, next) => {
+    console.error(err.message);
+    res.status(500).send('Internal Server Error');
+}                                                               
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
